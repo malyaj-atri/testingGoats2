88064ea3dfa1a520e6b39e94edd9f3d9f9e5704b
Malyaj Sirothia <sirothia@atrihub.io>
2023-03-14 12:13:29 -0700

    Feat: Chapter 4 TDD
    Committing changes from Chapter 4 Testing Goats for TDD training
    
    [ICTRAINING-1471]

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,19 +18,37 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        self.assertIn('To-do', self.browser.title)
+        header_text = self.browser.find_elements(By.TAG_NAME, 'h1')
+        self.assertIn('To-do', header_text[0].text)
 
         # She is invited to enter a to-do item straight away
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a To-do item'
+        )
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        input_box.send_keys('Buy peacock feathers')
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+                        "New To-do item did not appear in table")
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her list
 
 # The page updates again, and now shows both items on her list
 
