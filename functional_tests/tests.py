6edbbc86fb923e90071fd6c8fe0055c7202bf5d6
Malyaj Sirothia <sirothia@atrihub.io>
2023-03-15 14:23:13 -0700

    Feat: Chapter 6 TDD
    Committing changes from Chapter 6 Testing Goats for TDD training
    
    [ICTRAINING-1473]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
+from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import unittest
 
+MAX_WAIT = 10
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-do', self.browser.title)
@@ -42,8 +45,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -52,11 +54,10 @@ class NewVisitorTest(unittest.TestCase):
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -65,7 +66,19 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page updates again, and now shows both items on her list
 
-# The page updates again, and now shows both items on her list
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+            # The page updates again, and now shows both items on her list
 
 # Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some
@@ -74,6 +87,3 @@ class NewVisitorTest(unittest.TestCase):
 # She visits that URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep
-
-if __name__ == '__main__':
-    unittest.main()
