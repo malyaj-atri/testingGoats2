9e09438ee1d1a929e20c8d3a3183c0605ecf08ea
Malyaj Sirothia <sirothia@atrihub.io>
2023-03-15 17:54:33 -0700

    Feat: Chapter 7 TDD
    Committing changes from Chapter 7 Testing Goats for TDD training
    2nd commit of Chapter 7
    
    [ICTRAINING-1474]

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,4 +19,7 @@ from django.urls import re_path
 from lists import views
 
 app_name = 'home'
-urlpatterns = [re_path(r'^$', views.home_page, name='home')]
+urlpatterns = [
+    re_path(r'^$', views.home_page, name='home'),
+    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+]
